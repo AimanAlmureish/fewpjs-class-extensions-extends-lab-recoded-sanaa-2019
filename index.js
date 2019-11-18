d30e67c32916d07e9e1f26314cc47edd15c768aa
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-18 11:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+class Polygon {
+  constructor(array){
+    this.array = array 
+
+  }
+  
+  get countSides(){
+    return this.array.length
+  }
+  
+  get perimeter(){
+    let sum = 0;
+    this.array.forEach(item =>{
+      sum+=item;
+    })
+    return sum
+  }
+}
+
+
+class Triangle extends Polygon{
+  
+  constructor(array){
+    super(array)
+  }
+  
+            get isValid() {
+                if (this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]) {
+                    console.log(true);
+                    return true
+                } else {
+                    console.log(false);
+                    return false
+
+                }
+            }
+  
+}
+
+
+
+class Square extends Polygon{
+  constructor(array){
+    super(array)
+  }
+  
+  get isValid(){
+    this.array.forEach(item=>{
+      if(item == item){
+        return true
+      }
+    })
+  }
+}
